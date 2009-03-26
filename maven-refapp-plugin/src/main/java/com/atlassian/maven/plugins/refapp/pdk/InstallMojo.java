2a6964529698111b471902f3bdaec5203cf4ea17
dhornik <dhornik@atlassian.com>
2009-03-26 04:58:08 +0000

    Fixing mixed up annotations
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@31438 2c54a935-e501-0410-bc05-97a93f6bca70

--- maven-refapp-plugin/src/main/java/com/atlassian/maven/plugins/refapp/pdk/InstallMojo.java
+++ maven-refapp-plugin/src/main/java/com/atlassian/maven/plugins/refapp/pdk/InstallMojo.java
@@ -1,22 +1,17 @@
 package com.atlassian.maven.plugins.refapp.pdk;
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.PluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.execution.MavenSession;
-import org.apache.maven.artifact.Artifact;
-import com.atlassian.maven.plugins.refapp.pdk.AbstractPdkMojo;
+
 import com.atlassian.maven.plugins.refapp.MavenGoals;
 
 /**
- * @goal uninstall
+ * @goal install
  */
 public class InstallMojo extends AbstractPdkMojo {
 
     public void execute() throws MojoExecutionException, MojoFailureException {
-        MavenGoals goals = new MavenGoals(project, session, pluginManager, getLog());
+        final MavenGoals goals = new MavenGoals(project, session, pluginManager, getLog());
 
         ensurePluginKeyExists();
         goals.installPlugin(pluginKey, httpPort);
--- maven-refapp-plugin/src/main/java/com/atlassian/maven/plugins/refapp/pdk/UninstallMojo.java
+++ maven-refapp-plugin/src/main/java/com/atlassian/maven/plugins/refapp/pdk/UninstallMojo.java
@@ -2,16 +2,16 @@ package com.atlassian.maven.plugins.refapp.pdk;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import com.atlassian.maven.plugins.refapp.pdk.AbstractPdkMojo;
+
 import com.atlassian.maven.plugins.refapp.MavenGoals;
 
 /**
- * @goal install
+ * @goal uninstall
  */
 public class UninstallMojo extends AbstractPdkMojo {
 
     public void execute() throws MojoExecutionException, MojoFailureException {
-        MavenGoals goals = new MavenGoals(project, session, pluginManager, getLog());
+        final MavenGoals goals = new MavenGoals(project, session, pluginManager, getLog());
 
         ensurePluginKeyExists();
         goals.uninstallPlugin(pluginKey, httpPort);
