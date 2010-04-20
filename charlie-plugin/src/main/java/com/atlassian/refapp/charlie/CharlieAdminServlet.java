f8c627dae51c2dfb9994c2359385091fc153b1bb
evzijst <evzijst@atlassian.com>
2010-04-20 06:22:07 +0000

    REFAPP-59: Add a project landing page for use in AppLinks
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@58533 2c54a935-e501-0410-bc05-97a93f6bca70

--- charlie-plugin/src/main/java/com/atlassian/refapp/charlie/CharlieAdminServlet.java
+++ charlie-plugin/src/main/java/com/atlassian/refapp/charlie/CharlieAdminServlet.java
@@ -3,35 +3,27 @@ package com.atlassian.refapp.charlie;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 import com.atlassian.templaterenderer.TemplateRenderer;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
 import java.io.IOException;
 import java.util.*;
 
-public class CharlieAdminServlet extends HttpServlet
+public class CharlieAdminServlet extends CharlieServlet
 {
-    private static final String CHARLIE_KEYS = "charlie.keys";
-    private static final String CHARLIE_NAME = "charlie.name";
-
-    private final PluginSettingsFactory pluginSettingsFactory;
-    private final TemplateRenderer templateRenderer;
-
-    public CharlieAdminServlet(PluginSettingsFactory pluginSettingsFactory, TemplateRenderer templateRenderer)
+    public CharlieAdminServlet(TemplateRenderer templateRenderer, PluginSettingsFactory pluginSettingsFactory)
     {
-        this.pluginSettingsFactory = pluginSettingsFactory;
-        this.templateRenderer = templateRenderer;
+        super(pluginSettingsFactory, templateRenderer);
     }
 
     @Override protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException
     {
-        Map<String, Object> context = new HashMap<String, Object>();
-        String delete = request.getParameter("delete");
+        final Map<String, Object> context = new HashMap<String, Object>();
+        final String delete = request.getParameter("delete");
         if (delete == null)
         {
-            Map<String, String> charlies = new HashMap<String, String>();
+            final Map<String, String> charlies = new HashMap<String, String>();
             for (String charlie : getCharlies())
             {
                 charlies.put(charlie, getCharlieName(charlie));
@@ -41,7 +33,7 @@ public class CharlieAdminServlet extends HttpServlet
         }
         else
         {
-            List<String> charlies = getCharlies();
+            final List<String> charlies = getCharlies();
             charlies.remove(delete);
             storeCharlies(charlies);
             response.sendRedirect(request.getRequestURL().toString());
@@ -51,44 +43,12 @@ public class CharlieAdminServlet extends HttpServlet
     @Override protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException
     {
-        String key = request.getParameter("key");
-        String name = request.getParameter("name");
-        List<String> charlies = getCharlies();
+        final String key = request.getParameter("key");
+        final String name = request.getParameter("name");
+        final List<String> charlies = getCharlies();
         charlies.add(key);
         storeCharlies(charlies);
         setCharlieName(key, name);
         response.sendRedirect(request.getRequestURL().toString());
     }
-
-    private void render(String template, Map<String, Object> context, HttpServletResponse response) throws IOException
-    {
-        response.setContentType("text/html");
-        templateRenderer.render(template, context, response.getWriter());
-    }
-
-    private List<String> getCharlies()
-    {
-        List<String> charlies = (List<String>) pluginSettingsFactory.createGlobalSettings().get(CHARLIE_KEYS);
-        if (charlies == null)
-        {
-            charlies = new ArrayList<String>();
-        }
-        return charlies;
-    }
-
-    private void storeCharlies(List<String> charlies)
-    {
-        pluginSettingsFactory.createGlobalSettings().put(CHARLIE_KEYS, charlies);
-    }
-
-    private String getCharlieName(String key)
-    {
-        return (String) pluginSettingsFactory.createSettingsForKey(key).get(CHARLIE_NAME);
-    }
-
-    private void setCharlieName(String key, String name)
-    {
-        pluginSettingsFactory.createSettingsForKey(key).put(CHARLIE_NAME, name);
-    }
-
 }
---
+++ charlie-plugin/src/main/java/com/atlassian/refapp/charlie/CharlieLandingPage.java
@@ -0,0 +1,43 @@
+package com.atlassian.refapp.charlie;
+
+import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
+import com.atlassian.templaterenderer.TemplateRenderer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @since   v2.5.0
+ */
+public class CharlieLandingPage extends CharlieServlet
+{
+    public CharlieLandingPage(TemplateRenderer templateRenderer, PluginSettingsFactory pluginSettingsFactory)
+    {
+        super(pluginSettingsFactory, templateRenderer);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException
+    {
+        final String key = getCharlieKeyFromPath(req);
+        final Map<String, Object> context = new HashMap<String, Object>();
+
+        if (getCharlies().contains(key)) {
+            context.put("charlieKey", key);
+            context.put("charlieName", getCharlieName(key));
+            render("/templates/charlie.vm", context, response);
+        } else {
+            response.sendError(404, "Charlie with key " + key + " does not exist.");
+        }
+    }
+
+    private String getCharlieKeyFromPath(HttpServletRequest req)
+    {
+        final String[] elements = req.getPathInfo().split("/");
+        return elements[elements.length - 1];
+    }
+}
