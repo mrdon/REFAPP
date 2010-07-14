aa252779e2e55abc8d6dc5163aa62290d5efe8cf
psongsiritat <psongsiritat@atlassian.com>
2010-07-14 09:51:58 +0000

    build script now reads configs from maven runtime
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@63890 2c54a935-e501-0410-bc05-97a93f6bca70

--- scripts/run_build.py
+++ scripts/run_build.py
@@ -5,11 +5,35 @@ supported_build_script_version = [0, 1, 1]
 ssh.setDebug(1)
 ssh.setEndOfLine("\n")
 
+#resolve all the configs
+svn_user = ""
+svn_passwd = ""
+for server in mavenSession.getSettings().getServers():
+    if server.getId() == "central":
+        svn_user = server.getUsername()
+        svn_passwd = server.getPassword()
+
+# detect project name and branch from scm setting in project pom
+scm_conn = mavenProject.getScm().getDeveloperConnection().rstrip("/")
+relative_path = scm_conn[scm_conn.find("/svn/")+5:]
+path_components = relative_path.split("/")
+if (len(path_components) == 2) and (path_components[1] == "trunk"):
+    raise RuntimeError, "trunk release not supported yet"
+elif (len(path_components) == 3) and (path_components[1] == "branches"):
+    # good to go
+    pass
+else:
+    raise RuntimeError, "unrecognized scm path in project pom"
+
+project_name = path_components[0]
+project_branch = path_components[2]
+refapp_branch = raw_input("refapp branch:")
+
 # connect to a remote host
-print "Enchanter:Connecting to ${server}"
-ssh.connect('${server}', None)
+print "Enchanter:Connecting to %s" % target_server
+ssh.connect(target_server, None)
 # wait instead of prompt *****************************************************************
-ssh.waitFor('${unix_prompt}');
+ssh.waitFor(unix_prompt);
 print "Enchanter:Connected"
 
 # check version
@@ -24,23 +48,23 @@ if (version != supported_build_script_version):
 # run the build by answering the command line questions.
 ssh.sendLine('python build_refapp.py');
 ssh.waitFor('svn username(leave blank=no user/passwd):')
-ssh.sendLine('${svn_user}');
+ssh.sendLine(svn_user);
 print "Enchanter:svn username entered"
 
 ssh.waitFor("svn passwd:");
-ssh.sendLine("${svn_passwd}");
+ssh.sendLine(svn_passwd);
 print "Enchanter:svn password entered"
 
 ssh.waitFor('project(blank for listing):');
-ssh.sendLine('${project_name}');
+ssh.sendLine(project_name);
 print "Enchanter:project name entered"
 
 ssh.waitFor('project branch(blank for listing):');
-ssh.sendLine('${project_branch}');
+ssh.sendLine(project_branch);
 print "Enchanter:project branch entered"
 
 ssh.waitFor('refapp branch(blank for listing):');
-ssh.sendLine('${refapp_branch}');
+ssh.sendLine(refapp_branch);
 print "Enchanter:refapp branch entered"
 
 ssh.setTimeout(600000);
