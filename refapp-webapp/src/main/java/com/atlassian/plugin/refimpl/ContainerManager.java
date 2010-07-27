83d003701642abfd969df0d2f4eb77ddd6c13a5d
dbrown <dbrown@atlassian.com>
2010-07-27 07:11:28 +0000

    Commons logging override not needed anymore
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@64738 2c54a935-e501-0410-bc05-97a93f6bca70

--- refapp-webapp/src/main/java/com/atlassian/plugin/refimpl/ContainerManager.java
+++ refapp-webapp/src/main/java/com/atlassian/plugin/refimpl/ContainerManager.java
@@ -123,7 +123,6 @@ public class ContainerManager
         scannerConfig.setPackageVersions(new HashMap<String,String>() {{
             put("javax.servlet", "2.5");
             put("javax.servlet.http", "2.5");
-            put("org.apache.commons.logging", "1.1.1");
         }});
         hostComponentProvider = new SimpleHostComponentProvider();
 
