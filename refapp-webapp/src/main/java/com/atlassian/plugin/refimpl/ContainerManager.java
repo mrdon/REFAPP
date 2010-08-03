87bb3ec1a715fe68575f411ef1daa58c74fb12a0
dbrown <dbrown@atlassian.com>
2010-08-03 04:37:15 +0000

    Moving sal-api to WEB-INF/lib in accordance with upgrade guide
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@65254 2c54a935-e501-0410-bc05-97a93f6bca70

--- refapp-webapp/src/main/java/com/atlassian/plugin/refimpl/ContainerManager.java
+++ refapp-webapp/src/main/java/com/atlassian/plugin/refimpl/ContainerManager.java
@@ -117,6 +117,7 @@ public class ContainerManager
         packageIncludes.add("org.dom4j*");
         packageIncludes.add("javax.servlet*");
         packageIncludes.add("com.google*");
+        packageIncludes.add("com.atlassian.sal*");
         packageIncludes.add("com.opensymphony.module.sitemesh*");
 
         scannerConfig.setPackageIncludes(packageIncludes);
