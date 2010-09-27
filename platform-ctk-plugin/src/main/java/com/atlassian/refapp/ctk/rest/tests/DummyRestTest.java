656e5bdfde9a0144eb8a3420add6e9082ad0bff8
dbrown <dbrown@atlassian.com>
2010-09-27 04:43:09 +0000

    REFAPP-131: Remove usage of Jersey Client in favor of sal
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@109006 2c54a935-e501-0410-bc05-97a93f6bca70

--- platform-ctk-plugin/src/main/java/com/atlassian/refapp/ctk/rest/tests/DummyRestTest.java
+++ platform-ctk-plugin/src/main/java/com/atlassian/refapp/ctk/rest/tests/DummyRestTest.java
@@ -1,13 +1,17 @@
 package com.atlassian.refapp.ctk.rest.tests;
 
 import java.net.URI;
+import java.util.Map;
 
 import com.atlassian.functest.junit.SpringAwareTestCase;
 import com.atlassian.refapp.ctk.AppSpecificInfoProvider;
 import com.atlassian.refapp.ctk.rest.DummyRestResource;
 
-import com.sun.jersey.api.client.Client;
-
+import com.atlassian.sal.api.net.Request;
+import com.atlassian.sal.api.net.RequestFactory;
+import com.atlassian.sal.api.net.Response;
+import com.atlassian.sal.api.net.ResponseException;
+import com.atlassian.sal.api.net.ReturningResponseHandler;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -16,6 +20,7 @@ import static org.junit.Assert.assertNotNull;
 public class DummyRestTest extends SpringAwareTestCase
 {
     private AppSpecificInfoProvider infoProvider;
+    private RequestFactory requestFactory;
 
  	public void setInfoProvider(AppSpecificInfoProvider infoProvider)
  	{
@@ -23,20 +28,45 @@ public class DummyRestTest extends SpringAwareTestCase
  	}
 
     @Test
-    public void testWithDummyRestNoParam()
+    public void testWithDummyRestNoParam() throws ResponseException
     {
         URI uri = infoProvider.createLocalhostUriBuilder().path("rest").path("platform-ctk").path("latest").path("dummy").path("hello").build();
-        DummyRestResource.Result result = Client.create().resource(uri).get(DummyRestResource.Result.class);
+        DummyRestResource.Result result = get(uri, DummyRestResource.Result.class);
         assertNotNull(result);
         assertEquals("hello world!", result.getMessage());
     }
 
     @Test
-    public void testWithDummyRestWithParam()
+    public void testWithDummyRestWithParam() throws ResponseException
     {
         URI uri = infoProvider.createLocalhostUriBuilder().path("rest").path("platform-ctk").path("latest").path("dummy").path("hello").build();
-        DummyRestResource.Result result = Client.create().resource(uri).queryParam("who", "atlassian").get(DummyRestResource.Result.class);
+        DummyRestResource.Result result = get(uri, DummyRestResource.Result.class, "who", "atlassian");
         assertNotNull(result);
         assertEquals("hello atlassian!", result.getMessage());
+    }
+
+    public void setRequestFactory(RequestFactory requestFactory)
+    {
+        this.requestFactory = requestFactory;
+    }
+
+    private <R> R get(URI uri, final Class<R> resultClass, String... queryParams)
+            throws ResponseException
+    {
+        Request request = requestFactory.createRequest(Request.MethodType.GET, uri.toString());
+        if (queryParams != null)
+        {
+            request.addRequestParameters(queryParams);
+        }
+        Object result = request.executeAndReturn(
+                new ReturningResponseHandler()
+                {
+                    public Object handle(Response response) throws ResponseException
+                    {
+                        return response.getEntity(resultClass);
+                    }
+                }
+        );
+        return (R) result;
     }
 }
