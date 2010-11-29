7cb23999baff605547290eb452feb7570beb7c46
cjerozal <cjerozal@atlassian.com>
2010-11-29 17:49:46 +0000

    CR-REFAPP-81 adding comment to explain test
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@111934 2c54a935-e501-0410-bc05-97a93f6bca70

--- platform-ctk-plugin/src/test/java/it/com/atlassian/refapp/ctk/TestClient.java
+++ platform-ctk-plugin/src/test/java/it/com/atlassian/refapp/ctk/TestClient.java
@@ -1,12 +1,15 @@
 package it.com.atlassian.refapp.ctk;
 
-import com.atlassian.functest.rest.TestResults;
-import com.sun.jersey.api.client.Client;
-import org.junit.Test;
-
-import javax.ws.rs.core.UriBuilder;
 import java.io.File;
 import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import com.atlassian.functest.rest.TestResults;
+
+import com.sun.jersey.api.client.Client;
+
+import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNotNull;
@@ -27,6 +30,6 @@ public class TestClient
                 .path("rest").path("functest").path("1.0").path("junit").path("runTests").build();
         TestResults results = Client.create().resource(uri).queryParam("outdir", targetDir.getAbsolutePath()).get(TestResults.class);
         assertNotNull(results);
-        assertEquals(0, results.result);
+        assertEquals(0, results.result); // make sure that the number of failing tests is 0
     }
 }
