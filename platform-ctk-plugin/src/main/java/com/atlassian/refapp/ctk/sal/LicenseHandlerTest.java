b4e5ca303c664c5cecfc04375d9111ea4c1b1b5b
ahempel <ahempel@atlassian.com>
2011-02-10 00:44:31 +0000

    Test license is now an evaluation license, as Confluence TESTING licenses must be evaluation licenses.
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@118157 2c54a935-e501-0410-bc05-97a93f6bca70

--- platform-ctk-plugin/src/main/java/com/atlassian/refapp/ctk/sal/LicenseHandlerTest.java
+++ platform-ctk-plugin/src/main/java/com/atlassian/refapp/ctk/sal/LicenseHandlerTest.java
@@ -12,18 +12,18 @@ import static org.junit.Assert.assertNotNull;
 public class LicenseHandlerTest extends SpringAwareTestCase
 {
     static final String VALID_LICENSE =
-            "AAACUg0ODAoPeNqlVtFu2jAUfecrIu05iMA0pEqRRiFq0wFFNP0AYy6tp8RGtsPG3y9xYgiJHW/sM\n" +
-            "bFzz73nHJ/4y4pRbwHYG4+8IHj4On0YTbzoLSmeg9Fgh7IdY8MlwUAFRL+PhJ8XSEK4mTwP3oCfg\n" +
-            "MeL8HEbzfzJt5cn/2U03fpxMJ0OMGe/9sM5ByQJo+qbsqIfjP3xqFjNMdmlMJwzKhGWa5RBONtzg\n" +
-            "qj3DNkR0sFPwpEGTs5HUFuSorN4/VRXLz4kJwglz+FacYO4pMDV9nb/e1I2E76vl/EqTqLF4EDEJ\n" +
-            "5z72qiQXvkHokSoWcKZTJEQxZ4raHRCaV6tHlAqGu2sEKESKKK4TZ/GvmlYTW1BU2v2epjRwy0l5\n" +
-            "QsbgzUz6zzbAX89PKrHJcMonX0AlSL0g6qAHVD1Y1W4AW76bgECc3JUI9bbvAPj3mVcb5MiWbzJv\n" +
-            "Hnyw0tASHFl1VxYYd5ZuFS5o2JNkkFdZW6HaXqkUus9TrUYvkaMisppiBTkd6TnGmKWVRT0+adpE\n" +
-            "PVCO+BdAK9Ur5rLOf5EAtq61n3aZK884WxTe9/hzvu0LClwBo+FI92YzQp91JpNqSs2iTcevi1kT\n" +
-            "ELj9F0y0jpLKVQ/au/x7EivxrApfzOjNUn727HQ1+LjakUlpdv0mqlOAY3rSE2H2Y3/mX9o6y4b6\n" +
-            "4PmhKk32mZoZ2bnrGmO/iM47alVYxvM75DEcTG4yc5KQ0OadndZotwQgvo6YaG1dXP4i6wznzb7n\n" +
-            "Jf7gSOJrQWMKVNoSBsxf5foqvEOk38AE42n2DAtAhUAgpy4Zks9LuAOEFWQuG755vGTVfQCFFl4N\n" +
-            "NjkEYiw6SSVkFCVeLalMtUEX02rn";
+            "AAACVA0ODAoPeNqdVk1vozAQvfMrkPZMBE20h0pI2ya0i5qv3dBLb4ZMEq/ARLbJbv79gsHlywbRU\n" +
+            "xR/zJt5783gb8ElM18gNB3bdJzHuf04/256h8B8sB3HCFESpulsjSMgDLx/V0zvK8TB3c9/GgegN\n" +
+            "6D+yn1eLDaWvdjZ1sfh4816dfa/jIimf4+zJQXEcUrEnSKiZT9Yjp3vZhEOY5gtU8JRxLcoAdcnH\n" +
+            "M5UnDcDQInxB1MksYP7FcSpIE/O375WAPldfAOX0wzqoHtEOQEqjndLOOIivvu+XfsbP/BWxgmzC\n" +
+            "9xHMinBdvSMCGZi3X3iMWIMI1LjejcUZ+VuO6ENwnlEgkjU5VCit1IWdWvAxJ4+XpSSU5uUYkHHY\n" +
+            "cXNNktCoLvTs/i7TiMUP52BcOZaThlADyjy0crcAFfdWwGLKL6KEqtj5iml5j5GPP9NzGXwZnJgH\n" +
+            "JNzzaY6oMCaGLAQtStaxUlfS+HncZMMaCP2B8yp8XgF6uWRYxfXqNYRbuwHktaYRWlSsjBknaY3x\n" +
+            "IIU/50BLQUv08xodEEMupJWGesUL+0wIWHZACMWnSZsQcLo6NGwJBPS+GKIW7UxZcAm88rG+w1Jy\n" +
+            "qHReZ9DUltKodQw6mBr9rQXZeikb9WonaPD6Wjo6/BRe1EoOcX/krNeKJnByOwc8b3ye/OlBCc5W\n" +
+            "nbdBKDqiq6e7jTtNZ7k6wsjVT/OKkxFK4zIMv5OaM3VUkrFpO2fUg96xVSUjwsNoZ13xKThp+7Dw\n" +
+            "Zo/Hw4jc3oohnIK5bqSxndgkvwi7S6h/wEUMrYWMCwCFFBwk1DQL0qqxQbV15RZa/W8ESB7AhQ3b\n" +
+            "E9VSUvHT1S7vDj6p4zb0Oabdw==X02rr";
 
     static final String INVALID_LICENSE = StringUtils.repeat("Helloworld ", 50);
 
