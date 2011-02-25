54b4148b40b8ad5f223ac5e5f7c926564e908a48
jwalton <jwalton@atlassian.com>
2011-02-25 05:45:27 +0000

    REFAPP-224: Tests for DecoratorServlet's version getting.
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@119072 2c54a935-e501-0410-bc05-97a93f6bca70

---
+++ refapp-plugins/decorator-plugin/src/test/java/com/atlassian/refapp/decorator/DecoratorServletTest.java
@@ -0,0 +1,60 @@
+package com.atlassian.refapp.decorator;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.atlassian.templaterenderer.TemplateRenderer;
+
+import com.opensymphony.module.sitemesh.Page;
+import com.opensymphony.module.sitemesh.RequestConstants;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnit44Runner;
+
+@RunWith(MockitoJUnit44Runner.class)
+public class DecoratorServletTest
+{
+    @Mock TemplateRenderer templateRenderer;
+    @Mock HttpServletRequest request;
+    @Mock HttpServletResponse response;
+    @Mock ServletConfig servletConfig;
+    @Mock ServletContext servletContext;
+    @Mock Page page;
+    
+    @Before
+    public final void setUpMockBehaviour()
+    {
+        Mockito.when(request.getAttribute(RequestConstants.PAGE)).thenReturn(page);
+        Mockito.when(servletConfig.getServletContext()).thenReturn(servletContext);
+    }
+    
+    @Test
+    public void noExceptionsAreThrownWhenPropertiesAreNotAvailable() throws ServletException, IOException
+    {
+        DecoratorServlet ds = new DecoratorServlet(templateRenderer);
+        ds.init(servletConfig);
+        ds.service(request, response);
+    }
+    
+    @Test
+    public void exceptionsAreNotPropagatedWhenStreamReadFails() throws ServletException, IOException
+    {
+        InputStream immediatelyFailingInputStream = Mockito.mock(InputStream.class);
+        Mockito.when(immediatelyFailingInputStream.read()).thenThrow(new IOException());
+        
+        Mockito.when(servletContext.getResourceAsStream(Mockito.anyString())).thenReturn(immediatelyFailingInputStream);
+        DecoratorServlet ds = new DecoratorServlet(templateRenderer);
+        ds.init(servletConfig);
+        ds.service(request, response);
+    }
+}
