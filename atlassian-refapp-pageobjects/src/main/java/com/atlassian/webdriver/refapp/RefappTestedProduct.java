c859fe13c42b88f666f5b8ec5438f386bd65222d
psongsiritat <psongsiritat@atlassian.com>
2011-08-17 02:48:41 +0000

    REFAPP-253: removed unneeded pageobjects-elements dependency
    git-svn-id: https://studio.atlassian.com/svn/REFAPP/trunk@128501 2c54a935-e501-0410-bc05-97a93f6bca70

--- atlassian-refapp-pageobjects/src/main/java/com/atlassian/webdriver/refapp/RefappTestedProduct.java
+++ atlassian-refapp-pageobjects/src/main/java/com/atlassian/webdriver/refapp/RefappTestedProduct.java
@@ -9,8 +9,6 @@ import com.atlassian.pageobjects.TestedProductFactory;
 import com.atlassian.pageobjects.binder.InjectPageBinder;
 import com.atlassian.pageobjects.binder.StandardModule;
 import com.atlassian.pageobjects.component.Header;
-import com.atlassian.pageobjects.elements.ElementModule;
-import com.atlassian.pageobjects.elements.timeout.TimeoutsModule;
 import com.atlassian.pageobjects.page.AdminHomePage;
 import com.atlassian.pageobjects.page.HomePage;
 import com.atlassian.pageobjects.page.LoginPage;
@@ -51,7 +49,7 @@ public class RefappTestedProduct implements TestedProduct<WebDriverTester>
         this.webDriverTester = tester;
         this.productInstance = productInstance;
         this.pageBinder = new InjectPageBinder(productInstance, tester, new StandardModule(this),
-                new AtlassianWebDriverModule(this), new ElementModule(), new TimeoutsModule());
+                new AtlassianWebDriverModule(this));
 
         this.pageBinder.override(Header.class, RefappHeader.class);
         this.pageBinder.override(HomePage.class, RefappHomePage.class);
